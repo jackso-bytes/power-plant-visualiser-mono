05b449e9fbbe68510ce8751a27bbf664f2a875a2
jackso-bytes <tom.jackson2@sainsburys.co.uk>
2025-10-09 08:12:00 +0100

    feat: add powerTypes endpoint

---
+++ app/api/getAllPowerTypes/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '../../generated/prisma';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const powerTypes = await prisma.powerPlant.findMany({
+    select: {
+      primary_fuel: true,
+    },
+    distinct: ['primary_fuel'],
+  });
+
+  const powerTypesAsStringArray = powerTypes.map((p) => p.primary_fuel);
+
+  return Response.json({ powerTypesAsStringArray });
+}
