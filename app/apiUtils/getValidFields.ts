a618984363fd9531cdc10b3830a394f64cf105e6
jackso-bytes <tom.jackson2@sainsburys.co.uk>
2025-10-10 08:27:38 +0100

    feat: add util to filter fields from FE QS based on prisma schema

---
+++ app/apiUtils/getValidFields.ts
@@ -0,0 +1,19 @@
+import { Prisma } from '../generated/prisma';
+
+type PowerPlantField = keyof typeof Prisma.PowerPlantScalarFieldEnum;
+
+export const getValidFields = (
+  fields: string
+): PowerPlantField[] | undefined => {
+  if (fields.length === 0) return undefined;
+
+  /// split string and trim whitespace then dedup with set and convert back to array
+  const fieldsArray = Array.from(
+    new Set(fields.split(',').map((f) => f.trim()))
+  );
+  const validFieldsArray = fieldsArray.filter((f): f is PowerPlantField => {
+    return f in Prisma.PowerPlantScalarFieldEnum;
+  });
+
+  return validFieldsArray.length > 0 ? validFieldsArray : undefined;
+};
