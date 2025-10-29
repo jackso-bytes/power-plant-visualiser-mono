6869685faf56b125298e5b1d0c7ab98d86b8bd51
jackso-bytes <tom.jackson2@sainsburys.co.uk>
2025-10-29 07:40:37 +0000

    test: add skeleton of useGetAllCountries test

---
+++ hooks/useGetAllCountries/useGetAllCountries.test.tsx
@@ -0,0 +1,7 @@
+describe('useGetAllCountries test suite', () => {
+  it('Should render without crashing', () => {
+    // Test implementation goes here
+  });
+
+  it('Should return list of countries given a valid input', () => {});
+});
